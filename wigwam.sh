02ffa1d4a865e51bd101aed0739b669d5145e7dc
Wojciech Sznapka <wojtek@sznapka.pl>
2009-07-01 22:41:37 +0200

    Conky option which colors output to be used with execpi. Also error handling upgraded

--- wigwam.sh
+++ wigwam.sh
@@ -14,6 +14,7 @@
 #   -value (wartość indeksu)
 #   -both (zmiana procentowa i wartość indeksu)
 #   -sign (znak zmiany)
+#   -conky (kolorowanie składni w zależności od wartości indexu)
 #
 # Przykłady użycia:
 #   ./wigwam.sh WIG20 -percent
@@ -25,14 +26,34 @@
 # @author Wojciech Sznapka
 # @date 2009-07-01
 
+usage="Usage $0 index_name [-percent][-value][-both][-sign][-conky]"
+
 if [ -z $1 ]; then
-  echo
-  echo "Usage $0 index_name [-percent][-value][-both][-sign]"
+  echo $usage
+  exit
 fi
+
+if [ ${1:0:1} == "-" ]; then
+  echo Please type an index name first
+  echo $usage
+  exit
+fi
+
 base=`wget -qO- http://m.gpw.pl/ | egrep -o "$1&nbsp;[^%]+?[0-9\.]+?%[^\.]*[0-9\.]+"`
 percent=`echo $base |egrep -o "[-\+]?[0-9\.]+%"`
 value=`echo $base |egrep -o "[0-9\. ]+$"`
-echo $@  | grep '\-percent' > /dev/null && echo $percent
-echo $@  | grep '\-value'   > /dev/null && echo $value
-echo $@  | grep '\-both'    > /dev/null && echo $percent $value
-echo $@  | grep '\-sign'    > /dev/null && echo ${percent:0:1}
+sign=${percent:0:1}
+color=""
+
+if echo $@  | grep '\-conky' > /dev/null; then
+  if [ $sign == "-" ]; then
+    color='${color red}'
+  else
+    color='${color green}'
+  fi
+fi
+
+echo $@  | grep '\-percent' > /dev/null &&  echo $color$percent
+echo $@  | grep '\-value'   > /dev/null &&  echo $color$value
+echo $@  | grep '\-both'    > /dev/null &&  echo $color$percent $value
+echo $@  | grep '\-sign'    > /dev/null &&  echo $color$sign
